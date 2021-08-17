9e9c1441e24e97b908864eb3465f82f84f91ac3e
Marcus Vinicius de França Gomes <mgomes@cpqi.com>
2021-08-17 15:05:10 -0300

    Deploy.

---
+++ DatabaseCommands.cs
@@ -0,0 +1,167 @@
+using System;
+using MySql.Data.MySqlClient; 
+
+namespace C_
+
+{
+    public class DatabaseCommands
+    {
+        
+        
+        public void conectionDatabase() {
+            string cs = @"server=localhost;userid=root;password=root;database=Projetos;sslmode=none"; 
+            using var con = new MySqlConnection(cs); 
+            con.Open(); 
+
+            try
+            {
+                Console.WriteLine("Conectando com o banco de dados.\n"); 
+            } catch (Exception e) {
+                Console.WriteLine("ERRO: ", e.Message); 
+            } finally {
+                con.Close(); 
+            }
+                
+        }
+
+        
+        public void setUserDatabase(string user, string password) {
+            MySqlConnection myConnection = new MySqlConnection("server=localhost;port=3306;database=Projetos;user id=root;pwd=root;sslmode=none;");
+            string myInsertQuery = "INSERT INTO crud_USER (usuario, senha) Values('"+user+"', '"+password+"')"; 
+            MySqlCommand myCommand = new MySqlCommand(myInsertQuery);    
+            myCommand.Connection = myConnection;
+            
+            try {
+                myConnection.Open();
+                myCommand.ExecuteNonQuery();
+            } catch (Exception e) {
+                Console.Error.WriteLine("Error: " + e.Message);
+            } finally {
+                myCommand.Connection.Close();
+            }
+            
+            
+           
+        } 
+
+        
+        public void selectAllUserDatabase() {
+            MySqlConnection conexao;
+            MySqlCommand comando;
+            MySqlDataReader dataReader;
+
+            
+            conexao = new MySqlConnection("server=localhost;port=3306;database=Projetos;user id=root;pwd=root;sslmode=none;");
+            comando = new MySqlCommand("SELECT id, usuario, senha FROM crud_USER", conexao);
+            
+            try {
+                conexao.Open();
+                dataReader = comando.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    string id = Convert.ToString(dataReader["id"]);
+                    string usuario = Convert.ToString(dataReader["usuario"]);
+                    string senha = Convert.ToString(dataReader["senha"]);
+
+                    Console.WriteLine($"Id: {id} | Usuario: {usuario} | Senha: {senha}");
+                }
+            } catch (Exception e) {
+                Console.Error.WriteLine("ERRO: ", e.Message);
+            } finally {
+                conexao.Clone();
+            }
+            
+                
+        }
+
+        public void selectByIdUserDatabase(int idMysql) {
+            MySqlConnection conexao;
+            MySqlCommand comando;
+            MySqlDataReader dataReader;
+
+            conexao = new MySqlConnection("server=localhost;port=3306;database=Projetos;user id=root;pwd=root;sslmode=none;");
+            comando = new MySqlCommand($"SELECT * FROM crud_USER where id = {idMysql}", conexao);
+
+            try {
+                conexao.Open();
+                dataReader = comando.ExecuteReader();
+
+                while(dataReader.Read()) {
+                    string id = Convert.ToString(dataReader["id"]);
+                    string usuario = Convert.ToString(dataReader["usuario"]);
+                    string senha = Convert.ToString(dataReader["senha"]);
+
+                    Console.WriteLine($"Id: {id} | Usuário: {usuario} | Senha: {senha}");
+                }    
+            } catch (Exception e) {
+                Console.Error.WriteLine("ERRO: ", e.Message);
+            } finally {
+                conexao.Close();
+            }
+      
+        }
+
+        public void deteleUserDatabase()
+        {
+            MySqlConnection conexao;
+            MySqlCommand comando;
+
+            conexao = new MySqlConnection("server=localhost;port=3306;database=Projetos;user id=root;pwd=root;sslmode=none;");
+            string myInsertQuery = $"DELETE FROM crud_USER";
+            comando = new MySqlCommand(myInsertQuery);
+            comando.Connection = conexao;
+            try {
+                conexao.Open();
+                comando.ExecuteNonQuery();    
+            } catch (Exception e) {
+                Console.Error.WriteLine("ERRO: ", e.Message);
+            } finally {
+                comando.Connection.Close();
+            }
+            
+            
+        }
+        
+        
+        public void deleteByIdDatabase(int delete){
+            MySqlConnection conexao;
+            MySqlCommand comando;
+            MySqlDataReader dataReader;
+            conexao = new MySqlConnection("server=localhost;port=3306;database=Projetos;user id=root;pwd=root;sslmode=none;");
+
+            try{
+                comando = new MySqlCommand($"Delete FROM crud_USER where id = {delete}", conexao);
+                conexao.Open();
+                dataReader = comando.ExecuteReader();
+            
+
+                while(dataReader.Read()) {
+                    string id = Convert.ToString(dataReader["id"]);
+                    string usuario = Convert.ToString(dataReader["usuario"]);
+                    string senha = Convert.ToString(dataReader["senha"]);
+
+                    Console.WriteLine($"Id: {id} | Usuário: {usuario} | Senha: {senha}");
+                }
+            
+            }catch(Exception e){
+                Console.Error.WriteLine("Error: Eh duro programador" + e.Message);
+            }
+            finally{
+                conexao.Close();
+                
+            }
+        }
+
+         public void updateByIdDatabase(int update, string user, string password){
+            MySqlConnection conexao;
+            MySqlCommand comando;
+            MySqlDataReader dataReader;
+            conexao = new MySqlConnection("server=localhost;port=3306;database=Projetos;user id=root;pwd=root;sslmode=none;");
+            comando = new MySqlCommand($"UPDATE crud_USER SET usuario = '{user}', senha = '{password}' where id = {update}", conexao);
+            conexao.Open();
+            dataReader = comando.ExecuteReader();
+         }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte op;
+             
+            do {
+                Console.WriteLine("\nBIG DATABASE: 1 - Inserir | 2 - Selecionar todos  | 3 - Selecionar por Id| 4 - deletar tabela | 5 - deletar por id | 6 - atualizar dados | 0 - Sair do Banco \n");
+                Console.Write("Escolha >>> ");
+                op = byte.Parse(Console.ReadLine());
+
+                DatabaseCommands dc = new DatabaseCommands();
+                dc.conectionDatabase();
+
+
+                switch (op)
+                {
+                    case 0:
+                    {
+                        Console.WriteLine("Saindo...");
+                        break;
+                    }
+                    case 1:
+                        {
+                            Console.WriteLine("Insira Usuário: ");
+                            string usuario = Console.ReadLine();
+
+                            Console.WriteLine("Insira Senha: ");
+                            string senha = Console.ReadLine();
+
+                            dc.setUserDatabase(usuario, senha);
+                            Console.WriteLine("------ DATABASE -------------------");
+                            dc.selectAllUserDatabase();
+                            Console.WriteLine("-----------------------------------");
+                            Console.WriteLine("Usuário inserido no banco de dados");
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            dc.selectAllUserDatabase();
+                            Console.WriteLine("Usuários selecionados com sucesso!");
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            Console.WriteLine("Digite o id do usuário: " + "\n");
+                            int id = int.Parse(Console.ReadLine());
+                            dc.selectByIdUserDatabase(id);
+                            Console.WriteLine("Usuário selecionado com sucesso!");
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            Console.WriteLine("------ DATABASE -------------------");
+                            dc.selectAllUserDatabase();
+                            Console.WriteLine("-----------------------------------");
+                            
+                            Console.WriteLine("Você tem certeza que quer deletar a tabela inteira? (S/N) ");
+                            string resposta = Console.ReadLine();
+                            
+                            if(resposta == "S" || resposta == "s") {
+                                dc.deteleUserDatabase();
+                                Console.WriteLine("Tabela deletada com sucesso!");
+                            } else {
+                                Console.WriteLine("OK, tabela não excluída.");
+                            }
+
+                            break;
+                        }
+
+                    case 5:
+                        {
+                            Console.WriteLine("------ DATABASE -------------------");
+                            dc.selectAllUserDatabase();
+                            Console.WriteLine("-----------------------------------");
+
+                            Console.WriteLine("\nDigite o id do usuário: ");
+                            int id = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Você tem certeza que quer deletar o(a) usuário(a)? (S/N) ");
+                            string resposta = Console.ReadLine();
+                            if(resposta == "S" || resposta == "s") {
+                                dc.deleteByIdDatabase(id);
+                                Console.WriteLine("Usuário deletado com sucesso!");
+                            } else {
+                                Console.WriteLine("OK, usuário(a) não excluído(a).");
+                            }
+                
+                            break;
+                        }
+                    case 6:
+                    {
+                        Console.WriteLine("------ DATABASE -------------------");
+                        dc.selectAllUserDatabase();
+                        Console.WriteLine("-----------------------------------");
+                       
+                        Console.WriteLine("Insira o id de qual usuário que será atualizado");
+                        int id = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Insira um novo Usuário: ");
+                        string usuario = Console.ReadLine();
+                        Console.WriteLine("Insira uma novo Senha: ");
+                        string senha = Console.ReadLine();
+
+                        Console.WriteLine("Você tem certeza que quer alterar os dados usuário(a) e senha? (S/N) ");
+                        string resposta = Console.ReadLine();
+                        if(resposta == "s" || resposta == "S") {
+                            dc.updateByIdDatabase(id, usuario, senha);
+                            Console.WriteLine("Dados atualizados com sucesso!");
+                        } else {
+                            Console.WriteLine("OK, dado do(a) usuário(a) não alterado.");   
+                        }
+                        
+                        break;
+                    }
+                }
+            } while(op != 0);           
+        }
+    }
+}
